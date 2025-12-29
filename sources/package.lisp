573d2ec66d6143f4698224f97ee8e75651b63087
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2025-12-29 12:40:15 -0300

    New examples
    - New examples (nested tuplets and grace notes)
    
    - Included an error when OM version is inferior of 7.7 (no grace notes support)

--- sources/package.lisp
+++ sources/package.lisp
@@ -1,5 +1,11 @@
 (IN-PACKAGE :CL-USER)
 
+(let ((om-version (read-from-string (subseq om::*version-str* 0 3))))
+ (print (concatenate 'string "Openmusic version: " om::*version-str*))
+ (unless (>= om-version 7.7)
+  (om::om-message-dialog "ERROR: OMSIB library requires Openmusic 7.7 or higher.")
+  (om::om-abort)))
+  
 (defpackage :om-sibelius
  (:nicknames :omsib)
  (:use :cl))
